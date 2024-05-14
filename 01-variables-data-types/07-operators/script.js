f1c4f8e3539f2641651fc52609306668ad89a427
Gabriel Xavier Antunes <xavier.gabriel178@gmail.com>
2024-05-14 14:18:24 -0300

    att04

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,54 @@
+// Operadores Aritimeticos
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenaçao
+x='Hello' + '' + 'Word'
+
+// Exponenciação
+x = 2 ** 3;
+
+// Increment
+
+x=1
+
+//x=x+1
+x++
+
+// Decrement
+x = x -1;
+x--
+
+// Operadores de Atribuição
+
+x=10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+
+// Operadores de comparaçao
+
+x = 2 == '2';
+
+x = 2 === '2';
+
+x = 2 != '2';
+
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
