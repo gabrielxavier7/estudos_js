f9d32013c87a7c4bb6121bb5c93b2b7fc5e065fa
Gabriel Xavier Antunes <xavier.gabriel178@gmail.com>
2024-05-14 17:56:42 -0300

    att06

---
+++ 01-variables-data-types/09-strings/script.js
@@ -0,0 +1,46 @@
+let x;
+
+const name = 'John'
+const age = 30;
+
+x = 'Hello, my name is' + name  + ' and I am ' + age + ' years old';
+
+//Teplate Literals
+x = `Hello, my name is ${name}`;
+
+//String Properties and Methods
+const s = new String ('Hello Word');
+
+
+x= typeof s;
+
+x = s.length;
+
+// Access value by key
+x = s[1];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf('e')
+
+x = s.substring(7);
+
+x = s.slice(-11, -6);
+
+x = s.trim(-11, -6);
+
+x= s.replace();
+
+x = s.includes();
+
+x = s.valueOf();
+
+x = split('');
+
+
+console.log(x);
